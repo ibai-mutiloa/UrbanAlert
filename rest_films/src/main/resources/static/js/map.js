8a71c2235ad493849b3a3b385acc07eb336a8288
Muti_03 <85963350+Muti9289@users.noreply.github.com>
2025-05-29 12:42:10 +0200

    Separando en html mapa heatmap grafikas...

--- rest_films/src/main/resources/static/js/map.js
+++ rest_films/src/main/resources/static/js/map.js
@@ -1,12 +1,10 @@
-// index.js
 document.addEventListener('DOMContentLoaded', function () {
-  const btnLondres = document.getElementById('btn-londres');
-  const linkGrafikak = document.getElementById('link-grafikak');
-  const inicio = document.getElementById('inicio');
-  const mapaContainer = document.getElementById('mapa-container');
-  const grafikak = document.getElementById('grafikak');
+  let map = L.map('map').setView([51.5074, -0.1278], 10);
 
-  let map;
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+  }).addTo(map);
+
   let markers = new Map();
 
   const icons = {
@@ -15,20 +13,6 @@ document.addEventListener('DOMContentLoaded', function () {
     'vandalismo': L.icon({ iconUrl: 'icons/vandalismo-icon.png', iconSize: [25, 41], iconAnchor: [12, 41], popupAnchor: [1, -34] }),
     'default': L.icon({ iconUrl: 'icons/default-icon.png', iconSize: [25, 41], iconAnchor: [12, 41], popupAnchor: [1, -34] })
   };
-
-  btnLondres?.addEventListener('click', () => {
-    inicio.style.display = 'none';
-    mapaContainer.style.display = 'block';
-    grafikak.style.display = 'none';
-
-    map = L.map('map').setView([51.5074, -0.1278], 10);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; OpenStreetMap contributors'
-    }).addTo(map);
-
-    fetchAndAddCrimes();
-    setInterval(fetchAndAddCrimes, 10000);
-  });
 
   function fetchAndAddCrimes() {
     fetch('crimeservice/all')
@@ -47,15 +31,6 @@ document.addEventListener('DOMContentLoaded', function () {
       .catch(err => console.error('Error fetching crimes:', err));
   }
 
-  linkGrafikak?.addEventListener('click', function (e) {
-    e.preventDefault();
-    inicio.style.display = 'none';
-    mapaContainer.style.display = 'none';
-    grafikak.style.display = 'block';
-
-    // Llamada a la función que está en chart.js
-    if (typeof fetchCrimeDataAndRenderChart === 'function') {
-      fetchCrimeDataAndRenderChart();
-    }
-  });
+  fetchAndAddCrimes();
+  setInterval(fetchAndAddCrimes, 10000);
 });
